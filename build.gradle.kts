077b68f40bf346686354962c244e6d87b5b81755
Rush-iam <rushiam@gmail.com>
2026-04-08 21:39:24 -0400

    Update AGP 8.7 -> 8.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     val spatialsdkVersion by extra("0.11.1")
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
         classpath("com.bugsnag:bugsnag-android-gradle-plugin:8.2.0")
         classpath("com.meta.spatial.plugin:com.meta.spatial.plugin.gradle.plugin:${spatialsdkVersion}")
@@ -12,7 +12,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.meta.spatial.plugin") version "0.11.1" apply true
     id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply true
